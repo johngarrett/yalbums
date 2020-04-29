8f43299d5558206a01f00ac998485ab1f14ba49f
John Garrett <jacgarrett18@gmail.com>
2020-04-28 21:24:10 -0400

    add pydub

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
 import youtube_dl
 import urllib.parse as urlparse
+from pydub import AudioSegment
+
+save_location = './download/temp.mp3'
 
 def download_video(url):
     ydl_opts = {
         'format': 'bestaudio/best',
         'logtostderr': True,
+        'outtmpl': './download/temp.mp3', # './download/%(title)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -13,7 +17,34 @@ def download_video(url):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+def get_songs():
+    print("Enter/paste the timestamps. Press Ctrl+D ( or Ctrl+Z on windows ) when finished")
+    print("FORMAT: ##:## Song Name")
+    songs = {}
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        time, name = line.split(' ', 1)
+        songs[int(time.replace(':', '')) * 100] = name # seconds
+    
+    for time, name in songs.items():
+        print(f'{name} @ {time}')
+    return songs
 
+def split_download(songs):
+    source_file = AudioSegment.from_mp3(save_location)
+    ranges = []
+    for time in songs:
+        ranges.append(time)
+
+    for i in range(ranges):
+        if i == len(ranges) - 1:
+            song_file = source_file[ranges[i]:]
+        else:
+            song_file = source_file[ranges[i]: ranges[i+1]]
+        song_file.export(f'./output/{songs[range[i]]}.mp3')
 
 if __name__ == '__main__':
     # skip getting title and description for now
@@ -22,3 +53,5 @@ if __name__ == '__main__':
     video_id = urlparse.parse_qs(url_data.query)["v"][0]
     print(f'video id: {video_id}')
     download_video(url)
+    songs = get_songs()
+    split_download(songs)
