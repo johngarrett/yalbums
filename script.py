87b866aad88e055174619af023e5b8dac4be875b
John Garrett <jacgarrett18@gmail.com>
2020-05-02 20:56:39 -0400

    parse description

--- script.py
+++ script.py
@@ -1,8 +1,33 @@
 import youtube_dl
 import urllib.parse as urlparse
 from pydub import AudioSegment
+import re
 
 save_location = './download/temp.mp3'
+
+def yes_or_no(question):
+    while "the answer is invalid":
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[0] == 'y':
+            return True
+        if reply[0] == 'n':
+            return False
+
+def get_songs_from(description):
+    songs = {}
+    parsed_description = re.findall('(\d+:\d+) (.+)', description)
+    print('From the description, we found the following:')
+    for time, name in parsed_description:
+        mins, seconds = time.split(':')
+        #TODO: handle seconds being zero
+        songs[(int(mins) * 60 + int(seconds)) * 1000] = name.replace('/', '\u2215')
+        print(f'{name} at {time}')
+    yes = yes_or_no('Do you want to manually input the songs instead?')
+    if yes:
+        return get_songs
+    else:
+        return songs
+
 
 def download_video(url):
     ydl_opts = {
@@ -16,7 +41,12 @@ def download_video(url):
         }],
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url)
+        print(info_dict.get('title'))
         ydl.download([url])
+        songs = get_songs_from(info_dict.get('description'))
+        split_download(songs)
+
 def get_songs():
     print("Enter/paste the timestamps. Press Ctrl+D ( or Ctrl+Z on windows ) when finished")
     print("FORMAT: ##:## Song Name")
@@ -28,6 +58,7 @@ def get_songs():
             break
         time, name = line.split(' ', 1)
         mins, seconds = time.split(':')
+        name.replace('/', '//')
         songs[(int(mins) * 60 + int(seconds)) * 1000] = name # mili seconds
     
     for time, name in songs.items():
@@ -50,13 +81,9 @@ def split_download(songs):
             print(f'From {ranges[i]} to {ranges[i+1]} for song {songs[ranges[i]]}')
             song_file = source_file[ranges[i]: ranges[i+1]]
             song_file.export(f'./output/{songs[ranges[i]]}.mp3', format='mp3', codec='mp3')
+def save_song(name):
+    urllib.urlretrieve("http://img.youtube.com/vi/ytvideo/0.jpg")
 
 if __name__ == '__main__':
-    # skip getting title and description for now
     url = input("Youtube video url: ")
-    url_data = urlparse.urlparse(url)
-    video_id = urlparse.parse_qs(url_data.query)["v"][0]
-    print(f'video id: {video_id}')
     download_video(url)
-    songs = get_songs()
-    split_download(songs)
